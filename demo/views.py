d99de710c8ee984e7815f91530a2186a4cdaed20
ShuvoHassan <shuvo.hassan@hotmail.com>
2020-07-08 12:38:13 +0600

    Playing with templates and views

--- demo/views.py
+++ demo/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import Book
+from django.shortcuts import render
 
 def first(request):
-    return HttpResponse('First message from views')
+    books = Book.objects.all()
+    return render(request, 'first_temp.html', {'books': books})
 
 class Another(View):
+
+    Books = Book.objects.filter(is_published=True)
+    output = ''
+    for book in Books:
+        output += f"This book title {book.title} is published<br>"
+
     def get(self, request):
-        return HttpResponse('This is another function inside class')
+        return HttpResponse(self.output)
